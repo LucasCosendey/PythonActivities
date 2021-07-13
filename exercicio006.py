8aeb384af747ca3f467b2be4a06fbfce0404a99e
LucasCosendey <lcosendey@gmail.com>
2021-07-13 07:33:47 -0300

    Operadores e media
    Um exercício de media e outro de operadores

---
+++ exercicio006.py
@@ -0,0 +1,5 @@
+v1 = int(input('Digite um número: '))
+dn = v1*2
+tn = v1*3
+raizn = v1**(1/2)
+print('O dobro do número é {} \no triplo é {} \ne a raiz quadrada é {:.2f}'.format(dn, tn, raizn))
---
+++ exercicio007.py
@@ -0,0 +1,5 @@
+n1 = float(input('digite a primeira nota do aluno: '))
+n2 = float(input('digite a segunda nota do aluno:'))
+m = (n1+n2)/2
+print('A media entre as duas notas é {:.1f}'.format(m))
+
