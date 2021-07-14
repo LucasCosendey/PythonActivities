a22865310dcdabe07deeb4dcb26ec085f2f6e380
LucasCosendey <lcosendey@gmail.com>
2021-07-14 07:05:46 -0300

    Exer do 8 ao 10
    Conversor de moedas, tabuada e conversor de distancia

---
+++ exercicio008.py
@@ -0,0 +1,3 @@
+v1 = float(input('Digite um valor em metro: '))
+
+print('O valor {}m em centimetros é {}cm e em milimetro {}mm'.format(v1, (v1*100), (v1*1000)))
---
+++ exercicio010.py
@@ -0,0 +1,3 @@
+v1 = float(input('Quanto você tem de dinheiro agora? R$'))
+dolares = v1/4.32
+print('Com essa quantidade vc pode comprar {:.2f} dólares'.format(dolares))
